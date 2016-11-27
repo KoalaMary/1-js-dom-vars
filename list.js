c63a036230002fbb3a81fc6c75b84e6e5ec651b3
KoalaMary <mariiabaeva@yandex.ru>
2016-11-27 22:17:30 +0300

    Initial

--- list.js
+++ list.js
@@ -1,2 +1,71 @@
 'use strict';
+const names = ['Вася', 'Дима', 'Катя', 'Петя', 'Оля', 'Дамир', 'Октябрина'];
+const womenNames = ['Катя', 'Оля', 'Октябрина'];
+const jobs = ['Слесарь', 'Менеджер', 'Премьер Министр', 'Фотограф', 'Актёр', 'Повар', 'Блоггер'];
+var list = [];
+var ol = document.querySelector('ol');
+var minAge = 20;
+var maxAge = 35;
+var minSalary = 20;
+var maxSalary = 120;
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createPerson(n) {
+    for (var i = 0; i < n; i++) {
+        var Person = {
+            name: names[getRandom(0, names.length - 1)],
+            age: getRandom(minAge, maxAge),
+            job: jobs[getRandom(0, jobs.length - 1)],
+            salary: getRandom(minSalary, maxSalary) * 1000
+        };
+        list.push(Person);
+    }
+}
+
+function addPerson(Person) {
+    var li = document.createElement('li');
+    var spanName = document.createElement('span');
+    spanName.innerText = Person.name;
+    li.appendChild(spanName);
+    li.appendChild(document.createTextNode(' ' + Person.age + ' ' + Person.job + ' ' + Person.salary));
+    checkSalary(Person.salary, li);
+    checkJob(Person.job, li);
+    checkAge(Person.age, spanName);
+    checkName(Person.name, li);
+    ol.appendChild(li);
+}
+
+function checkSalary(salary, li) {
+    if (salary < 50000)
+        li.style.backgroundColor = 'red';
+    if (salary >= 50000 && salary <= 80000)
+        li.style.backgroundColor = 'yellow';
+    if (salary > 80000)
+        li.style.backgroundColor = 'green';
+}
+
+function checkJob(job, li) {
+    if (job === 'Блоггер')
+        li.style.textDecoration = 'underline';
+}
+
+function checkAge(age, span) {
+    if (age >= 20 && age <= 27)
+        span.style.fontWeight = 'bold';
+}
+
+function checkName(name, li) {
+    womenNames.forEach(function (womanName, i, womenNames) {
+        if (name === womanName)
+            li.style.fontSize = '1.5em';
+    });
+}
+
+createPerson(10);
+list.forEach(function (Person, i, list) {
+    addPerson(Person);
+});
 
--- time.js
+++ time.js
@@ -1,2 +1,72 @@
 'use strict';
+var clock = document.querySelector('#clock');
+var date = new Date();
+
+function initializeClock() {
+    var args = Array.prototype.slice.call(arguments);
+    var month = date.getMonth() + 1;
+
+    function createSpan(spanName) {
+        var span = document.createElement('span');
+        span.className = spanName;
+        clock.appendChild(span);
+    }
+
+    for (var i = 0; i < args.length; i++) {
+        if (args[i] instanceof Date) {
+            date = args[i];
+            month = date.getMonth();
+        }
+        if (args[i] instanceof Array) {
+            var clockArgs = args[i];
+            clockArgs.forEach(function (item, i, clockArgs) {
+                createSpan(item);
+            });
+        }
+    }
+
+
+    if (document.querySelector('span') === null) {
+        createSpan('hours');
+        createSpan('minutes');
+        createSpan('seconds');
+    }
+
+    function updateClock(date) {
+        setInterval(function () {
+            var spans = document.querySelectorAll('span');
+            console.log(date.getFullYear());
+            spans.forEach(function (item, i, spans) {
+                switch (item.className) {
+                    case 'year':
+                        document.querySelector('.year').innerText = date.getFullYear();
+                        break;
+                    case 'month':
+                        document.querySelector('.month').innerText = month;
+                        break;
+                    case 'day':
+                        document.querySelector('.day').innerText = date.getDate();
+                        break;
+                    case 'hours':
+                        document.querySelector('.hours').innerText = date.getHours();
+                        break;
+                    case 'minutes':
+                        document.querySelector('.minutes').innerText = date.getMinutes();
+                        break;
+                    case 'seconds':
+                        document.querySelector('.seconds').innerText = date.getSeconds();
+                        break;
+                }
+            });
+            date.setSeconds(date.getSeconds() + 1);
+        }, 1000)
+    }
+
+    updateClock(date, []);
+}
+
+// initializeClock(new Date(2014, 6, 14));
+// initializeClock(['hours', 'seconds']);
+// initializeClock(new Date(2014, 6, 14), ['year', 'month', 'seconds']);
+initializeClock(['year', 'month', 'day', 'hours', 'minutes', 'seconds']);
 
